614f4f3263fe6af085618a7cd9d97f24f0a9bef2
Silmar <silmarjr2@gmail.com>
2023-03-22 13:30:28 -0300

    temporary fix to wpa_supplicant issue

--- hosts/behemoth/configuration.nix
+++ hosts/behemoth/configuration.nix
@@ -25,6 +25,12 @@
     enable = true;  # Enables wireless support via wpa_supplicant.
     fallbackToWPA2 = false;
     # TODO add declarative networks with SOPs 
+    networks = { 
+      # TODO remove dummy network
+      "DUMMY" = { # Dummy network to prevent wpa_supplicant failing to restart
+        psk = "DUMMYDUMMY";
+      };
+    };
 
     # imperative networks
     allowAuxiliaryImperativeNetworks = true;
