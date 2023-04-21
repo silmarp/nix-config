4843d35235f66277805be80e58d97d259caaaa6b
Silmar <silmarjr2@gmail.com>
2023-04-21 17:29:10 -0300

    add experimental features to bluetooth

--- hosts/pyrolusite/configuration.nix
+++ hosts/pyrolusite/configuration.nix
@@ -42,6 +42,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
   };
   services.blueman.enable = true;
 
