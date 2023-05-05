7b595ab94222219b2cdb5ab1d5080a544c75e720
Silmar <silmarjr2@gmail.com>
2023-05-05 16:12:52 -0300

    create global config for home-manager

---
+++ home/global/default.nix
@@ -0,0 +1,29 @@
+{ inputs, lib, pkgs, config, outputs, ... }:
+
+{
+  home = { 
+    username = "silmar";
+    homeDirectory = "/home/silmar";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+  };
+
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.git = {
+    enable = true;
+    diff-so-fancy.enable = true;
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
--- home/pyrolusite.nix
+++ home/pyrolusite.nix
@@ -3,10 +3,9 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "silmar";
-  home.homeDirectory = "/home/silmar";
-
   imports = [ 
+    ./global
+
     ./features/taskwarrior
     ./features/neovim
     ./features/sway
@@ -14,17 +13,14 @@
     ./features/alacritty
   ];
 
-  nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
       # TODO move wpa to its own module
       zsnes # TODO remove or move to games section
       htop
       firefox-wayland
-      librewolf-wayland
       qutebrowser
       discord
       wl-clipboard
-      bitwarden-cli
       rnix-lsp
       nodePackages.bash-language-server
       prismlauncher
@@ -38,44 +34,20 @@
       rssguard # RSS feed reader
       grim
       slurp
-      haskellPackages.Monadoro
       zellij
       zathura
       spotifyd
       spotify-tui
       kabmat
-      taskwarrior-tui
       steam
       lutris
       wine
       osu-lazer
       bitwarden
-      emacs
       ferdium
       (tor-browser-bundle-bin.override {
         useHardenedMalloc = false;
        })
-      gnome.pomodoro
       element-desktop
   ];
-
-  programs.git = {
-    enable = true;
-    diff-so-fancy.enable = true;
-  };
-
-  
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
 }
