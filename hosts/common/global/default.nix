dc86ba51fc53838994f0984ee1acd852cb27b50b
Silmar <silmarjr2@gmail.com>
2023-07-14 17:37:54 -0300

    generalize nix settings

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -2,4 +2,19 @@
   imports = [
     ./tailscale.nix
   ];
+
+  nix = {
+    settings = {
+      # Enable flakes and 'nix' command
+      experimental-features = "nix-command flakes";
+	    # Optimazes nix store
+	    auto-optimise-store = true;
+    };
+      # Garbage collection config
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
 }
--- hosts/pyrolusite/configuration.nix
+++ hosts/pyrolusite/configuration.nix
@@ -135,20 +135,6 @@
     ]; 
   };
 
-  nix = {
-      settings = {
-      	  # Enable flakes and 'nix' command
-          experimental-features = "nix-command flakes";
-	  # Optimazes nix store
-	  auto-optimise-store = true;
-      };
-      # Garbage collection config
-      gc = {
-          automatic = true;
-	  dates = "weekly";
-	  options = "--delete-older-than 7d";
-      };
-  };
 
   services.dbus.enable = true;
   xdg.portal = {
