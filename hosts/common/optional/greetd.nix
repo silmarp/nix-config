9e51f0bf7fd58ce4141439456c575e12157344a1
Silmar <silmarjr2@gmail.com>
2023-08-10 16:26:38 -0300

    change greeter to regreet

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,15 +1,29 @@
 { config, lib, pkgs, ... }:
 
 {
+  # TODO make custom theme import here
+  users.extraUsers.greeter.packages = [
+    pkgs.gnome.gnome-themes-extra
+    pkgs.materia-theme
+  ];
+
   services.greetd = {
     enable = true;
     settings = rec {
       initial_session = {
-      # TODO change greet package to ReGreet (or GTKGreet)
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -c sway -r --asterisks -t";
-        user = "silmar";
+        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet -l debug";
+        user = "greeter";
       };
       default_session = initial_session;
+    };
+  };
+  programs.regreet = {
+    enable = true;
+    settings = {
+      GTK = {
+        theme_name = "Materia-dark";
+        icon_theme_name = "Materia-dark";
+      };
     };
   };
 }
