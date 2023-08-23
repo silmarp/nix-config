4c25686f57ff699ffb47efec24edee8670e5462d
Silmar <silmarjr2@gmail.com>
2023-08-23 11:31:40 -0300

    start zk module

--- modules/home-manager/zk/default.nix
+++ modules/home-manager/zk/default.nix
@@ -1,3 +1,37 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.zk;
+  tomlFormat = pkgs.formats.toml { };
+in
+
 {
+  options.progrmas.zk = {
+    enable = lib.mkEnableOption "zk";
+    
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.zk;
+      defaultText = lib.literalExpression "";
+      description = "The package to use for the zk binary";
+    };
+
+    settings = lib.mkOption {
+      type = tomlFormat.type;
+      default = { };
+      example = pkgs.literalExpression ''
+      '';
+      description = ''
+      '';
+    };
+
+    # TODO make templates
+    # completion ?
+  };
+  
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."zk/config.toml" = lib.mkIf (cfg.settings != {}) {
+      source = tomlFormat.generate "config.toml" cfg.settings;
+    };
+  };
 }
