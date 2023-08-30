469a78e7ef1370a8fd37931f54c51e21bda97e0f
Silmar <silmarjr2@gmail.com>
2023-08-29 21:01:30 -0300

    minor tweaks to wofi

--- home/features/wofi/default.nix
+++ home/features/wofi/default.nix
@@ -3,7 +3,19 @@
 {
   programs.wofi = {
     enable = true;
-    # settings = {};
-    # style = '' '';
+    settings = {
+      location = 0; # Center
+      allow_images = true;
+      columns = 1;
+      insensitive = true;
+    };
+    style = /* css */ ''
+      #img {
+        margin: 5px;
+      }
+      .entry {
+        margin: 5px;
+      }
+    '';
   };
 }
