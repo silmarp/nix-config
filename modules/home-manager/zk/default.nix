012e66222b0cb4b516133c20dfccd418a0f033d1
Silmar <silmarjr2@gmail.com>
2023-09-07 22:41:43 -0300

    add example and description to zk module

--- modules/home-manager/zk/default.nix
+++ modules/home-manager/zk/default.nix
@@ -20,12 +20,26 @@ in
       type = tomlFormat.type;
       default = { };
       example = pkgs.literalExpression ''
+        note = {
+          language = "en";
+          default-title = "Untitled";
+          filename = "{{id}}-{{slug title}}";
+          extension = "md";
+          template = "default.md";
+          id-charset = "alphanum";
+          id-length = 4;
+          id-case = "lower";
+        };
+        extra = {
+          author = "Mickaël";
+        };
       '';
       description = ''
+        Configuration written to $XDG_CONFIG_HOME/zk/config.toml.
+
+        See https://github.com/mickael-menu/zk/blob/main/docs/config.md for available options and documentation.
       '';
     };
-
-    # completion ?
   };
   
   config = lib.mkIf cfg.enable {
