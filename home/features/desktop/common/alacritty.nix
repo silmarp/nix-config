cedec646964ff221b988fc504284e54f237c5411
Silmar <silmarjr2@gmail.com>
2023-09-18 16:58:46 -0300

    remove window opacity, add colors config to alacritty

--- home/features/desktop/common/alacritty.nix
+++ home/features/desktop/common/alacritty.nix
@@ -1,10 +1,41 @@
 { config, pkgs, lib, ... }:
 
+let
+  colors = config.colorScheme.colors;
+in
 {
   programs.alacritty = {
     enable = true;
     settings = {
-      window.opacity = 0.7;
+      colors = {
+        # Default colors
+        primary = {
+          background = "#${colors.base00}";
+          foreground = "#${colors.base05}";
+        };
+
+        # Normal colors
+        normal = {
+          black =   "#${colors.base04}";
+          red =     "#${colors.base0F}";
+          green =   "#${colors.base0B}";
+          blue =    "#${colors.base0C}";
+          magenta = "#${colors.base0E}";
+          cyan =    "#${colors.base0D}";
+          white =   "#${colors.base04}";
+        };
+
+        # Bright colors
+        bright = {
+          black =   "#${colors.base04}";
+          red =     "#${colors.base0F}";
+          green =   "#${colors.base0B}";
+          blue =    "#${colors.base0C}";
+          magenta = "#${colors.base0E}";
+          cyan =    "#${colors.base0D}";
+          white =   "#${colors.base04}";
+        };
+      };
     };
   };
 }
