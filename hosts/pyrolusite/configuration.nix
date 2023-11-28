855f63310fc804ef59c7cd53446057c4489d4726
Silmar <silmarjr2@gmail.com>
2023-11-28 16:02:18 -0300

    clean pyrolusite configuration

--- hosts/pyrolusite/configuration.nix
+++ hosts/pyrolusite/configuration.nix
@@ -1,18 +1,16 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
+
       ../common/global
+
       ../common/optional/greetd.nix
+      ../common/optional/wireless.nix
 
       ../common/users/silmar
-      ../common/optional/wireless.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -34,17 +32,9 @@
   };
   services.blueman.enable = true;
 
-
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
+  # TODO move to locale global config
   time.timeZone = "America/Sao_Paulo";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
       LC_TIME = "pt_BR.UTF-8";
@@ -60,7 +50,6 @@
     lidSwitchExternalPower = "suspend";
   };
 
-  # powerManagement.powertop.enable = true;
   
   # Enable Wayland windowing system
   security.polkit.enable = true; #sway privileges suport
@@ -73,24 +62,6 @@
     };
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = {
-  #   "eurosign:e";
-  #   "caps:escape" # map caps to escape.
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound = {
-  #     enable = true;
-  #     mediaKeys.enable = true;
-  # };
   security.rtkit.enable = true;
   services.pipewire = {
       enable = true;
@@ -100,10 +71,9 @@
       jack.enable = true;
       wireplumber.enable = true;
   };
+  
   # TODO Move Pam to its own config
   security.pam.services = { swaylock = { }; };
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   services.dbus.enable = true;
   xdg.portal = {
@@ -113,9 +83,7 @@
       xdg-desktop-portal-wlr
     ];
   };
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [];
+
   environment.pathsToLink = [ "/share/zsh" ];
 
   fonts.packages = with pkgs; [
@@ -128,37 +96,11 @@
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable docker
   virtualisation.docker.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = "22.05";
 
 }
 
