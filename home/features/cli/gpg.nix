fc46854f1865506e3fc7ac4a227c0be4089bf7da
Silmar <silmarjr2@gmail.com>
2023-12-23 01:53:41 -0300

    fix gpg pinentry error

--- home/features/cli/gpg.nix
+++ home/features/cli/gpg.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 
 {
-  home.packages = [ pkgs.pinentry-curses ];
+  home.packages = [ pkgs.pinentry-gtk2 ];
 
   programs.gpg = {
     enable = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "gtk2";
   };
 }
