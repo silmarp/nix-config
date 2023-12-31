85d0facfb87ec6209d2e9b27cdbd7b62e6b6a1ec
Silmar <silmarjr2@gmail.com>
2023-12-31 16:29:16 -0300

    update git config

--- home/features/cli/git.nix
+++ home/features/cli/git.nix
@@ -6,5 +6,12 @@
     diff-so-fancy.enable = true;
     userName = "Silmar";
     userEmail = "silmarjr2@gmail.com";
+    ignores = [ "result" ];
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+    aliases = {
+      graph = "log --graph";
+    };
   };
 }
