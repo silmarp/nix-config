01734670d599a1c52a5082e17a85351c7e8dd447
Silmar <silmarjr2@gmail.com>
2024-02-01 10:49:18 -0300

    remove wayland option from greetd

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -13,7 +13,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c Hyprland -c sway";
+        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.gtkgreet}/bin/gtkgreet -c Hyprland";
         user = "greeter";
       };
     };
