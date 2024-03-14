c6c791ad93e1e7dc0425303edd4bc1d3fe643d75
Silmar <silmarjr2@gmail.com>
2024-03-14 13:45:39 -0300

    changed layout of wlogout

--- home/features/desktop/common/waylandWM/wlogout.nix
+++ home/features/desktop/common/waylandWM/wlogout.nix
@@ -1,7 +1,28 @@
-{...}:
+{ ... }:
 
 {
   programs.wlogout = {
     enable = true;
+
+    layout = [
+      {
+        label = "lock";
+        action = "loginctl lock-session";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+    ];
   };
 }
