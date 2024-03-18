ce0355e7ae19f98aea7f21de6fdb3f3864a8e16a
Silmar <silmarjr2@gmail.com>
2024-03-18 17:20:21 -0300

    repo: create deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$#" -eq 0 ]; then
+    echo "No hosts to build"
+    exit
+fi
+
+for host in "$@"; do
+    echo "Building for $host"
+    nixos-rebuild switch --target-host $host --flake .\#$host --use-remote-sudo
+done
