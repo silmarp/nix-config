7f6635dab09adcb8193385db525da591a5f83fec
Silmar <silmarjr2@gmail.com>
2024-04-07 12:48:00 -0300

    sops: fix remote build and add rutile host key

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -5,5 +5,8 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
-  sops.age.keyFile = "/home/silmar/.config/sops/age/keys.txt";
+  #sops.age.keyFile = "/home/silmar/.config/sops/age/keys.txt";
+  sops.age.sshKeyPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+  ];
 }
