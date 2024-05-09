449909abac4f20c03b0c9c25e9b69fa69989985d
Silmar <silmarjr2@gmail.com>
2024-05-09 13:32:31 -0300

    flake: update

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/Hyprland";
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     };
 
     sops-nix.url = "github:Mic92/sops-nix";
