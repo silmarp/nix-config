c34da0a6ea790e2a9598cd7baca494ec9af131db
Silmar <silmarjr2@gmail.com>
2024-05-15 17:37:41 -0300

    neovim: add dashboard plugin

--- home/features/neovim/ui.nix
+++ home/features/neovim/ui.nix
@@ -163,6 +163,35 @@ in
           vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
         '';
       }
+      {
+        plugin = alpha-nvim;
+        type = "lua";
+        config = /*lua*/ ''
+          local alpha = require("alpha")
+          local dashboard = require("alpha.themes.dashboard")
+
+          dashboard.section.header.val = {
+                "                                                     ",
+                "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+                "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+                "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+                "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+                "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+                "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+                "                                                     ",
+          }
+          dashboard.section.header.opts.hl = "Title"
+
+          dashboard.section.buttons.val = {
+              dashboard.button( "n", "󰈔 New file" , ":enew<CR>"),
+              dashboard.button( "e", " Explore", ":NvimTreeToggle<CR>"),
+              dashboard.button( "f", "󰍉 Find files", ":Telescope find_files<CR>"),
+          }
+
+          alpha.setup(dashboard.opts)
+          vim.keymap.set("n", "<space>h", ":Alpha<CR>", { desc = "Open home dashboard" })
+        '';
+      }
 /*
       {
         plugin = which-key-nvim;
