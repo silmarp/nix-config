644827a6bcf72bd087f82569c8afd78f73b63140
Silmar <silmarjr2@gmail.com>
2024-07-09 09:20:56 -0300

    greetd: fix regreet coundn't find sessions

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,5 +1,13 @@
 { pkgs, lib, config, ... }:
 
+let 
+  homeCfgs = config.home-manager.users;
+  homeSharePaths = lib.mapAttrsToList (_: v: "${v.home.path}/share") homeCfgs;
+  sessionCommand = lib.getExe (pkgs.writeShellScriptBin "sessionCommand" ''
+     export XDG_DATA_DIRS="$XDG_DATA_DIRS:${lib.concatStringsSep ":" homeSharePaths}" GTK_USE_PORTAL=0
+     ${lib.getExe pkgs.cage} -m last -s -- ${lib.getExe config.programs.regreet.package}
+    '');
+in
 {
   users.extraUsers.greeter = {
     # Does not work without
@@ -11,12 +19,19 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${lib.getExe pkgs.cage} -m last -s -- ${lib.getExe config.programs.regreet.package}";
+        command = "${sessionCommand}";
         user = "greeter";
       };
     };
   };
+
   programs.regreet = {
     enable = true;
+    settings = {
+      background = {
+        path = "${homeCfgs.silmar.wallpaper}";
+        fit = "Cover";
+      };
+    };
   };
 }
