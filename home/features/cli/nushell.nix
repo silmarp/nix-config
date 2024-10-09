164c8627e422dafc3b8cca3b78d84c5aa8280106
Silmar <silmarjr2@gmail.com>
2024-10-08 21:11:05 -0300

    nushell: add base config

--- home/features/cli/nushell.nix
+++ home/features/cli/nushell.nix
@@ -10,6 +10,20 @@
     envFile.text = ''
       $env.EDITOR = 'nvim'
     '';
+    extraConfig = /* nu */ ''
+      let carapace_completer = {|spans|
+        carapace $spans.0 nushell $spans | from json
+      }
+      $env.config = {
+        show_banner: false,
+        completions: {
+          case_sensitive: false # case-sensitive completions
+          quick: true    # set to false to prevent auto-selecting completions
+          partial: true    # set to false to prevent partial filling of the prompt
+          algorithm: "fuzzy"    # prefix or fuzzy
+        }
+      } 
+    '';
   };
   
   programs.carapace = {
