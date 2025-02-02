8c595a43ad1e0e82d3aa99dc192fbab34c47b349
Silmar <silmarjr2@gmail.com>
2025-02-02 17:50:56 -0300

    hematite: add hyprland and git configs

--- home/hematite.nix
+++ home/hematite.nix
@@ -5,11 +5,21 @@
     ./global
 
     ./features/neovim
+
+    ./features/desktop/hyprland
     
     ./features/cli/nushell.nix
     ./features/cli/starship.nix
     ./features/cli/direnv.nix
+    ./features/cli/git.nix
+
+    ./features/productivity
   ];
+
+wallpaper = builtins.fetchurl {
+  url = "https://i.stack.imgur.com/cmoDG.jpg";
+  sha256 = "0f701igjzh8593if7z0hzx1aj7cy0c9i0vscs1pdcp16ibp2vy14";
+};
 
   home.packages = with pkgs; [
     inputs.nixgl.packages.x86_64-linux.nixGLIntel
