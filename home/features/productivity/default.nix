aa2db2038721fc220488f6519f0651dc9b54208f
Silmar <silmarjr2@gmail.com>
2025-08-25 17:49:41 -0300

    productivity: add nextcloud-client

--- home/features/productivity/default.nix
+++ home/features/productivity/default.nix
@@ -4,5 +4,6 @@
   imports = [
     ./zk.nix
     ./rbw.nix
+    ./nextcloud-client.nix
   ];
 }
---
+++ home/features/productivity/nextcloud-client.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  services.nextcloud-client = {
+    enable = true;
+    package = pkgs.nextcloud-client;
+    startInBackground = true;
+  };
+}
