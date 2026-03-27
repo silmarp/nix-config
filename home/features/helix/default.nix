db1bf49fd2964b92f69eb5d836f7506b2afad209
Silmar <silmarjr2@gmail.com>
2026-03-27 08:59:32 -0300

    helix: improve config

--- home/features/helix/default.nix
+++ home/features/helix/default.nix
@@ -8,6 +8,13 @@
       theme = "adwaita-dark";
       editor = {
         line-number = "relative";
+        soft-wrap.enable = true;
+        bufferline = "always";
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
       };
     };
   };
