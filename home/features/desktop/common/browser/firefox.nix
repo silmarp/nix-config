c13d0097907b5844e415aadb659e29a591c6d61b
Silmar <silmarjr2@gmail.com>
2026-08-17 21:21:40 -0300

    firefox: change config path to new model

--- home/features/desktop/common/browser/firefox.nix
+++ home/features/desktop/common/browser/firefox.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.firefox = {
     enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles.default = {
       isDefault = true;
       settings = {
