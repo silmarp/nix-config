d0067adf04d1abced5a8722be24e886242e6011b
Silmar <silmarjr2@gmail.com>
2026-08-20 21:59:53 -0300

    Revert "firefox: change config path to new model"
    This reverts commit c13d0097907b5844e415aadb659e29a591c6d61b.

--- home/features/desktop/common/browser/firefox.nix
+++ home/features/desktop/common/browser/firefox.nix
@@ -1,9 +1,8 @@
-{ config, ... }:
+{ ... }:
 
 {
   programs.firefox = {
     enable = true;
-    configPath = "${config.xdg.configHome}/mozilla/firefox";
     profiles.default = {
       isDefault = true;
       settings = {
